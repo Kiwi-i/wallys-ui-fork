5e8b356ab9775e1b247cf03a81d65b3ff1e7e255
Alexi <33795225+Kiwi-i@users.noreply.github.com>
2019-10-12 09:16:51 -0600

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -15,11 +15,12 @@ local defaults; do
                 
                 event:connect(function()
                     local input = frame.InputBegan:connect(function(key)
-                        if key.UserInputType == Enum.UserInputType.MouseButton1 then
-                            local objectPosition = Vector2.new(frame.AbsolutePosition.X, frame.AbsolutePosition.Y);
+                        if key.UserInputType == Enum.UserInputType.MouseButton1 then;
                             while heartbeat:wait() and inputService:IsMouseButtonPressed(Enum.UserInputType.MouseButton1) do
                                 pcall(function()
-                                    frame:TweenPosition(UDim2.new(0, mouse.X - objectPosition.X, 0, mouse.Y - objectPosition.Y), 'Out', 'Linear', 0.1, true);
+									local cummy = mouse.X - frame.AbsolutePosition.X
+									local scat = mouse.Y - frame.AbsolutePosition.Y
+                                    frame:TweenPosition(UDim2.new(0, mouse.X + cummy, 0, mouse.Y + scat), 'Out', 'Linear', 0.1, true);
                                 end)
                             end
                         end
