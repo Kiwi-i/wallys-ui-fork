a3ca71459cafea486d1c46e2b60557db9e60c6d8
Alexi <33795225+Kiwi-i@users.noreply.github.com>
2019-10-11 23:20:32 -0600

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -987,7 +987,7 @@ local defaults; do
         topcolor       = Color3.fromRGB(30, 30, 30);
         titlecolor     = Color3.fromRGB(255, 255, 255);
         
-        underlinecolor = Color3.fromRGB(0, 255, 140);
+        underlinecolor = Color3.fromRGB(255, 103, 250);
         bgcolor        = Color3.fromRGB(35, 35, 35);
         boxcolor       = Color3.fromRGB(35, 35, 35);
         btncolor       = Color3.fromRGB(25, 25, 25);
